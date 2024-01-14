13707c09b5c1fe3040abaf8f833f7e6f2be659b3
Jeffrey Burmood <Jeffrey-swdev@proton.me>
2024-01-14 13:43:01 -0700

    Create connectors.py

---
+++ data_server/connectors.py
@@ -0,0 +1,10 @@
+""" this file contains the methods used to connect and communicate with the various databases """
+
+import psycopg
+
+class PostgresDB:
+
+    def connect(self):
+        pass
+
+    
